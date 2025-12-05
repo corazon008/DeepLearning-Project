ddea8f3a601fde8213a225f7c75327eae7b17ceb
corazon <99136079+corazon008@users.noreply.github.com>
2025-12-05 09:53:48 +0100

    feat: Add base wrapper class

--- src/torch/torch_model.py
+++ src/torch/torch_model.py
@@ -18,16 +18,17 @@ class MyDropout:
 
 
 class RegressionModel(nn.Module):
-    def __init__(self, nb_features: int, loss_fn=nn.MSELoss(), width: int = 512, dropout_rates: List[float] = [0.0]):
+    def __init__(self, nb_features: int, layers:int=2, width: int = 512, dropout_rates: List[float] = [0.0], loss_fn=nn.MSELoss(), activation=nn.ReLU()):
         super().__init__()
         dropout = MyDropout(dropout_rates)
-        self.net = nn.Sequential(
-            nn.Linear(nb_features, width), nn.ReLU(),
-            nn.Dropout(dropout.get()),
-            nn.Linear(width, width), nn.ReLU(),
-            nn.Dropout(dropout.get()),
-            nn.Linear(width, 1)
-        )
+        self.net = nn.Sequential()
+        self.net.add_module("input_layer", nn.Linear(nb_features, width))
+        for i in range(layers):
+            self.net.add_module(f"hidden_layer_{i+1}", nn.Linear(width, width))
+            self.net.add_module(f"relu_{i+1}",activation)
+            self.net.add_module(f"dropout_{i+1}", nn.Dropout(dropout.get()))
+
+        self.net.add_module("output_layer", nn.Linear(width, 1))
         self.loss_fn = loss_fn
 
     def forward(self, x):
@@ -44,17 +45,17 @@ class RegressionModel(nn.Module):
         r2 = 1 - ss_res / ss_tot
         return {"MAE": mae, "R2": r2.item()}
 
-    def fit(self, X, y):
-        pass
-
 
 class ClassificationModel(nn.Module):
-    def __init__(self, nb_features: int, num_classes: int, loss_fn=nn.CrossEntropyLoss()):
+    def __init__(self, nb_features: int, num_classes: int, loss_fn=nn.CrossEntropyLoss(), width: int = 512, dropout_rates: List[float] = [0.0]):
         super().__init__()
+        dropout = MyDropout(dropout_rates)
         self.net = nn.Sequential(
-            nn.Linear(nb_features, 512), nn.ReLU(),
-            nn.Linear(512, 512), nn.ReLU(),
-            nn.Linear(512, num_classes)
+            nn.Linear(nb_features, width), nn.ReLU(),
+            nn.Dropout(dropout.get()),
+            nn.Linear(width, width), nn.ReLU(),
+            nn.Dropout(dropout.get()),
+            nn.Linear(width, 1)
         )
         self.loss_fn = loss_fn
 
--- src/torch/torch_wrapper.py
+++ src/torch/torch_wrapper.py
@@ -8,31 +8,34 @@ from src.torch.torch_model import RegressionModel
 device = "cpu"
 
 
-class TorchRegressor(BaseEstimator, RegressorMixin):
+class Skwrapper(BaseEstimator, RegressorMixin):
     def __init__(self,
                  nb_features=None,
                  width=128,
+                 layers=2,
                  dropout_rates=[0.0],
                  lr=1e-3,
+                 activation: nn.Module = nn.ReLU(),
+                 optimizer: optim.Optimizer = optim.Adam,
+                 loss_fn: nn.Module = nn.MSELoss(),
                  batch_size=32,
                  epochs=10, ):
 
         self.nb_features = nb_features
         self.width = width
+        self.layers = layers
         self.dropout_rates = dropout_rates
         self.lr = lr
+        self.activation = activation
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
         self.batch_size = batch_size
         self.epochs = epochs
-        self.device = device
 
         self.model = None
 
     def _build_model(self):
-        return RegressionModel(
-            nb_features=self.nb_features,
-            width=self.width,
-            dropout_rates=self.dropout_rates
-        ).to(self.device)
+        raise NotImplementedError
 
     def fit(self, X, y):
         X = torch.tensor(X, dtype=torch.float32)
@@ -45,11 +48,11 @@ class TorchRegressor(BaseEstimator, RegressorMixin):
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
 
         self.model = self._build_model()
-        optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        optimizer = self.optimizer(self.model.parameters(), lr=self.lr)
 
         for epoch in range(self.epochs):
             for batch_X, batch_y in dataloader:
-                batch_X, batch_y = batch_X.to(self.device), batch_y.to(self.device)
+                batch_X, batch_y = batch_X.to(device), batch_y.to(device)
 
                 preds = self.model(batch_X)
                 loss = self.model.compute_loss(preds, batch_y)
@@ -63,6 +66,43 @@ class TorchRegressor(BaseEstimator, RegressorMixin):
     def predict(self, X):
         self.model.eval()
         with torch.no_grad():
-            X = torch.tensor(X, dtype=torch.float32).to(self.device)
+            X = torch.tensor(X, dtype=torch.float32).to(device)
             preds = self.model(X)
         return preds.cpu().numpy().ravel()
+
+
+class TorchRegressor(Skwrapper):
+    def __init__(self,
+                 nb_features=None,
+                 width=128,
+                 layers=2,
+                 dropout_rates=[0.0],
+                 lr=1e-3,
+                 activation: nn.Module = nn.ReLU(),
+                 optimizer: optim.Optimizer = optim.Adam,
+                 loss_fn: nn.Module = nn.MSELoss(),
+                 batch_size=32,
+                 epochs=10, ):
+
+        super().__init__(
+            nb_features=nb_features,
+            width=width,
+            layers=layers,
+            dropout_rates=dropout_rates,
+            lr=lr,
+            optimizer=optimizer,
+            activation=activation,
+            loss_fn=loss_fn,
+            batch_size=batch_size,
+            epochs=epochs
+        )
+
+    def _build_model(self):
+        return RegressionModel(
+            nb_features=self.nb_features,
+            width=self.width,
+            layers=self.layers,
+            activation=self.activation,
+            loss_fn=self.loss_fn,
+            dropout_rates=self.dropout_rates
+        ).to(device)
